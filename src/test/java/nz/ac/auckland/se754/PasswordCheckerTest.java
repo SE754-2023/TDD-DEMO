16cd9f9f63661861b5edf060b28fb7776a53d655
Valerio Terragni <v.terragni@auckland.ac.nz>
2023-05-02 14:27:32 +1200

    test is failing

--- src/test/java/nz/ac/auckland/se754/PasswordCheckerTest.java
+++ src/test/java/nz/ac/auckland/se754/PasswordCheckerTest.java
@@ -1,5 +1,6 @@
 package nz.ac.auckland.se754;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
@@ -34,13 +35,18 @@ public class PasswordCheckerTest {
   @Test
   public void When_PwdAtLeast8Characters_Expect_Accepted
   () {
-    //arrange
     PasswordChecker checker = new PasswordChecker();
-    // act
     boolean result = checker.isValid("12345678");
-    // assert
     assertTrue(result);
   }
 
+  //A user should not be able to set a password of less than 8 characters
+  @Test
+  public void When_PwdWithLEssThan8Characters_Expect_Declined
+  () {
+    PasswordChecker checker = new PasswordChecker();
+    boolean result = checker.isValid("hello");
+    assertFalse(result);
+  }
 
 }
