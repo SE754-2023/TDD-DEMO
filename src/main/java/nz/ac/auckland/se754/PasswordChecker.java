fa9b328a6407f6a8862323304a940ed5a19fe9e3
Valerio Terragni <v.terragni@auckland.ac.nz>
2023-05-02 14:35:54 +1200

    test is passing

--- src/main/java/nz/ac/auckland/se754/PasswordChecker.java
+++ src/main/java/nz/ac/auckland/se754/PasswordChecker.java
@@ -3,7 +3,7 @@ package nz.ac.auckland.se754;
 public class PasswordChecker {
 
   public Boolean isValid(String s, User user) {
-    return s.length() >= 8;
+    return s.length() >= 8 && !s.contains(user.getName());
   }
 
 
