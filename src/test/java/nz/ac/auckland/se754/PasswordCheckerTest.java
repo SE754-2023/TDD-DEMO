5f21473087d813de568598a0efb87868e371b360
Valerio Terragni <v.terragni@auckland.ac.nz>
2023-05-02 14:45:37 +1200

    test is failing

--- src/test/java/nz/ac/auckland/se754/PasswordCheckerTest.java
+++ src/test/java/nz/ac/auckland/se754/PasswordCheckerTest.java
@@ -3,6 +3,7 @@ package nz.ac.auckland.se754;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 /**
@@ -27,16 +28,25 @@ import org.junit.jupiter.api.Test;
  * <p>
  * - A user should not be able to set a password of less than 8 characters [NEGATIVE]
  * <p>
- * .....
+ * Requirement Passwords must contain at least three of the four available character types:
+ * lowercase letters (a-z), uppercase letters (A-Z), numbers (0-9), and symbols (!@#$%). .....
  */
 public class PasswordCheckerTest {
+
+  private User user;
+  private PasswordChecker checker;
+
+  @BeforeEach
+  public void setUp() {
+    user = new User("bob");
+    checker = new PasswordChecker();
+  }
+
 
   //  A user should be able to set a password of at least 8 characters [POSITIVE]
   @Test
   public void When_PwdAtLeast8Characters_Expect_Accepted
   () {
-    User user = new User("bob");
-    PasswordChecker checker = new PasswordChecker();
     boolean result = checker.isValid("12345678", user);
     assertTrue(result);
   }
@@ -45,18 +55,25 @@ public class PasswordCheckerTest {
   @Test
   public void When_PwdWithLEssThan8Characters_Expect_Declined
   () {
-    User user = new User("bob");
-
-    PasswordChecker checker = new PasswordChecker();
     boolean result = checker.isValid("hello", user);
     assertFalse(result);
   }
 
   @Test
   public void When_PwdDoesNotContainUserName_Expect_Declined() {
-    User user = new User("bob");
-    PasswordChecker checker = new PasswordChecker();
     boolean result = checker.isValid("bob12345678", user);
+    assertFalse(result);
+  }
+
+  @Test
+  public void When_PwdDoesNotContainUserName_Expect_Accepted() {
+    boolean result = checker.isValid("12345678", user);
+    assertTrue(result);
+  }
+
+  @Test
+  public void When_PwdContainsNoneCharacterTypes_Expect_Declined() {
+    boolean result = checker.isValid("((((((((((()))))))))))", user);
     assertFalse(result);
   }
 
